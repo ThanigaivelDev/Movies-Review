c45fb6702652dc12427ceada0b41552254b87c78
Thanigaivel .V <thanigaivelvaithiyanathan@gmail.com>
2024-12-25 08:16:14 +0530

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/MovieController.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+	@Autowired
+	public MovieService movieservice;
+	
+	@GetMapping
+	public ResponseEntity<List<Movie>> getallMovies() {
+		return new ResponseEntity<List<Movie>>(movieservice.allMovies(), HttpStatus.OK);
+	}
+	
+	@GetMapping("/{imdbId}")
+	public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId) {
+		return new ResponseEntity<Optional<Movie>>(movieservice.singleMovie(imdbId), HttpStatus.OK);
+		
+		
+	}
+
+}
